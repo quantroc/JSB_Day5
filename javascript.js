b966971749dd18f78fc257a439f2a48e501680af
QuanNKD <quannkd@bap.jp>
2023-01-09 21:52:37 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,84 @@
+//number
+let a = 2.2
+console.log(a);
+let b = 5
+console.log(b);
+// string
+let c = "hello world"
+console.log(c);
+
+//boolean co hai gia tri la true va false
+let d = true
+console.log(d);
+
+//null
+let e = null
+console.log(e);
+
+//undefined khong thuoc kieu du lieu nao
+let f = undefined
+console.log(f);
+
+//array là kiểu mảng
+let arr = [5,6,3,7,8]
+        // 0,1,2,3,4 
+//mảng có năm phần từ và bắt đầu từ vị trí 0
+console.log(arr.length); // .length để truy xuất số phẩn tử của một mảng
+//truy xuất phần tử trong mảng:
+let g = arr[4]
+console.log(g);
+console.log(arr);
+// dùng hàm push() để thêm phần tử vào cuối mảng
+let h = 6
+arr.push(h)
+console.log(arr);
+
+//object
+// gồm thuộc tính và giá trị( key và value)
+let person = {
+    name: 'quan',
+    age: 25,
+}
+console.log(person);
+
+// =>> cách khai báo biến : let
+// =>> 5 kiểu dữ liệu cơ bản : Number, string , boolean , null, undefined, array, object
+// =>> javascript là ngôn ngữ để giao tiếp với máy
+
+
+// Toán tử (operator)
+// + 
+let a1 = 5
+let a2 = 6
+// = là để gán giá trị cho một biến
+let b1 = a1 + a2
+console.log(b1);
+// -
+let b2 = a2 - a1
+console.log(b2);
+
+// * : nhân
+// /
+let c1 = 10/3
+console.log(c1);
+
+// % phép chia có dư
+let d1 = 10%3
+console.log(d1);
+// toán tử ++ và --
+let a3 = a1 + 1
+let a4 = a1++
+let a5 = ++a1
+console.log(a5);
+console.log(a4);
+
+
+
+
+
+
+
+
+
+
+
